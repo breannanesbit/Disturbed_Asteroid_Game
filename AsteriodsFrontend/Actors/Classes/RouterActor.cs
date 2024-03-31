61111347a9beed23a63786a4de1ea98b326107c1
bobby <bobby@DESKTOP-6V2GR6M>
2024-03-30 23:18:32 -0600

    Router

---
+++ AsteriodsFrontend/Actors/Classes/RouterActor.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actors.Classes;
+public class RouterActor :ReceiveActor
+{
+    private readonly IActorRef router;
+    public RouterActor()
+    {
+        router = Context.ActorOf(Props.Empty.WithRouter(new ConsistentHashingPool(5)), "hashing-pool-router");
+
+        Receive<string>(key =>
+        {
+            router.Tell(new ConsistentHashableEnvelope(message: key, hashKey: key));
+        });
+    }
+
+}
---
+++ AsteriodsFrontend/Actors/Classes/SomeActor.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actors.Classes;
+
+public class SomeActor :ReceiveActor
+{
+    public SomeActor()
+    {
+        Receive<string>(message =>
+        {
+            Sender.Tell(Self.Path.ToString());
+        });
+    }
+}
