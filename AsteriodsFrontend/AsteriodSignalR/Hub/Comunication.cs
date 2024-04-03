db80036372c73f51104ad278c8376525bdfc0f59
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-04-03 12:26:03 -0600

    added signal r project

---
+++ AsteriodsFrontend/AsteriodSignalR/Hub/Comunication.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRAPI.Hub
+{
+    public class ComunicationHub : DynamicHub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
