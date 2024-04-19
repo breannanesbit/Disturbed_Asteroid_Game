ba360ef65c1bea02ab943a718fc735cb5b39d1f7
Breanna Nesbit <brebrenesbit02@gmail.com>
2024-04-19 14:47:36 -0600

    logs appeared in grafana

--- AsteriodsFrontend/AsteriodWeb/Program.cs
+++ AsteriodsFrontend/AsteriodWeb/Program.cs
@@ -5,6 +5,7 @@ using OpenTelemetry.Trace;
 using Serilog;
 using Serilog.Exceptions;
 using Serilog.Sinks.Grafana.Loki;
+using Serilog.Sinks.Loki;
 using Shared;
 using Shared.SignalRService;
 using System.Reflection;
@@ -43,6 +44,7 @@ builder.Host.UseSerilog((context, loggerConfig) =>
         .Enrich.WithProperty("job", "your-api-job")
         .Enrich.WithExceptionDetails()
         .WriteTo.Console()
+        .WriteTo.LokiHttp("http://loki:3100")
         .WriteTo.GrafanaLoki("http://loki:3100");
 });
 
